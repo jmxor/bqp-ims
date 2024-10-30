25f08c574d5732ae64d9bc8dab7d034f5f12d9ec
jmxor <jamesmrippeth@gmail.com>
2024-10-30 18:12:08 +0000

    Add nested not-found page for dashboard routes

---
+++ src/app/dashboard/[...not-found]/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <main className="flex flex-col items-center justify-center gap-4 px-4 py-3 grow">
+      <h1>Page Not Found!</h1>
+      <button
+        onClick={() => router.back()}
+        className="px-4 py-2 text-white bg-blue-500 rounded-md">
+        Go Back
+      </button>
+    </main>
+  );
+}
